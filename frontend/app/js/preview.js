021a83fce777785831d7fcb07eb63a5fab3e1e07
Nathan Wilson <nswilson@gmail.com>
2016-12-18 01:10:06 -0500

    Use highlightjs on the frontend

--- frontend/app/js/preview.js
+++ frontend/app/js/preview.js
@@ -1,6 +1,14 @@
 $(function() {
   $('.preview-swap').each(function () {
-    $(this).html($(this).next().html());
+    var index = $(this).data('index');
+    var rougeBlock = $('#file-' + index + ' td.code pre');
+    rougeBlock.html('<code class="code-block-hljs" id="code-block-' + index + '" />');
+    var codeBlock = $('#code-block-' + index);
+    codeBlock.text($('#submission-code-' + index).text());
+    codeBlock.each(function (i, block) {
+      hljs.highlightBlock(block);
+    });
+    $(this).html(codeBlock.html());
   });
 
   $('.iterations-nav-item').hover(function() {
@@ -8,16 +16,23 @@ $(function() {
       var files = $(this).data('solution');
 
       files.forEach(function(file, index) {
-        var currentText = $('#submission-code-' + index).text();
-        var differ = new WikEdDiff();
-        var diffHtml = differ.diff(file[1], currentText);
-        $('#file-' + index + ' .code').html(diffHtml);
+       // var currentText = $('#submission-code-' + index).text();
+        var codeBlock = $('#code-block-' + index);
+        codeBlock.text(file[1]);
+        codeBlock.each(function(i,b) {
+          hljs.highlightBlock(b);
+        });
+       // var differ = new WikEdDiff();
+       // var diffHtml = differ.diff(file[1], currentText);
+       // $('#file-' + index + ' .code').html(diffHtml);
       });
     }
   }, function() {
     if (!$(this).hasClass('active')) {
       $('.preview-swap').each(function () {
-        $(this).next().html($(this).html());
+        var index = $(this).data('index');
+        var codeBlock = $('#code-block-' + index);
+        codeBlock.html($(this).html());
       });
     }
   });
