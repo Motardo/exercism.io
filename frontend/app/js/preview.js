576b0c73af066598d3875d45dc840a18e35c43f0
Nathan Wilson <nswilson@gmail.com>
2016-12-19 09:06:08 -0500

    Use sass to style diffs

--- frontend/app/js/preview.js
+++ frontend/app/js/preview.js
@@ -43,11 +43,11 @@ $(function() {
     btnShowDiff.on('click', function(e) {
       e.preventDefault();
       var activeTab = $('.iterations-nav-item.active');
-      var linenums = $('.submission-code-body pre.lineno a');
+//      var linenums = $('.submission-code-body pre.lineno a');
       if ($(this).hasClass('active')) {
         iterationsNavItemInactive.removeClass('diffed-old');
-        activeTab.removeClass('diffed-new');
-        linenums.css('color', '#999999');
+//        activeTab.removeClass('diffed-new');
+//        linenums.css('color', '#999999');
         restoreCodeBlocks();
       } else {
         var otherTab = activeTab.prev('.iterations-nav-item');
@@ -65,10 +65,11 @@ $(function() {
           $('#code-block-' + index).html(preInnerHtml);
         });
         otherTab.addClass('diffed-old');
-        activeTab.addClass('diffed-new');
-        linenums.css('color', '#f84');
+//        activeTab.addClass('diffed-new');
+//        linenums.css('color', '#f84');
       }
-      $('.btn-show-diff').toggleClass('active');
+//      $('.btn-show-diff').toggleClass('active');
+      $('#submission').toggleClass('diffed');
     });
   }
 });
