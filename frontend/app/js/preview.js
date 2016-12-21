8036b67a045099259739e365e5a44d259d82fd0f
Nathan Wilson <nswilson@gmail.com>
2016-12-21 11:19:37 -0500

    normalize track for hljs

--- frontend/app/js/preview.js
+++ frontend/app/js/preview.js
@@ -9,16 +9,19 @@
         $codeBlocks,
         $codeCache,
         diffCodeBlock,
+        findAdjacentTab,
         highlightCodeBlock,
         $iterationsNavItemInactive,
+        normalizeTrack,
         restoreCodeBlocks,
-        $submission;
+        $submission,
+        toggleDiffView;
     
 
     $submission = $('#submission');
+    $codeBlocks = $('.submission-code-body td.code');
     $activeTab = $('.iterations-nav-item.active');
     activeSolution = $activeTab.data('solution');
-    $codeBlocks = $('.submission-code-body td.code');
 
     highlightCodeBlock = function(file, index) {
       var codeBlock = $codeBlocks.eq(index).find('code');
@@ -39,9 +42,20 @@
       });
     };
 
+    normalizeTrack = function (track) {
+      var hljsNames = {
+        'csharp': 'cs',
+        'objective-c': 'objectivec',
+        'perl5': 'perl'
+      };
+      return hljsNames[track] || track;
+    };
+
+    // Highlight the code and cache a copy of the highlighted block for quickly
+    // restoring the view after a diff or preview of another iteration
     $codeCache = $('<div id="code-cache" class="hidden">').appendTo($('#current_submission'));
     $codeBlocks.each(function (index, block) {
-      var language = $('#file-' + index).data('track');
+      var language = normalizeTrack($('#file-' + index).data('track'));
       $(this).html('<pre><code class="lang-' + language + '">');
       $(this).addClass('lang-' + language);
       $(this).find('code').text(activeSolution[index][1]);
@@ -54,12 +68,15 @@
 
     $iterationsNavItemInactive.hover(function() {
       if ($submission.hasClass('diff-view')) {
+        // When there are more two iterations, allow selecting which iteration
+        // to diff against by hovering over the inactive tabs
         if (!$(this).hasClass('diff-view-old')) {
           $iterationsNavItemInactive.removeClass('diff-view-old');
           $(this).data('solution').forEach(diffCodeBlock);
           $(this).addClass('diff-view-old');
         }
       } else {
+        // Allow previews of the other iterations by hovering over their tabs
         $(this).data('solution').forEach(highlightCodeBlock);
       }
     }, function() {
@@ -68,24 +85,37 @@
       }
     });
 
+    // Only enable diff button when there is more than 1 iteration
     if ($iterationsNavItemInactive.length > 0) {
       $('.btn-show-diff')
       .removeClass('disabled')
       .on('click', function(e) {
         e.preventDefault();
-        if ($submission.hasClass('diff-view')) {
-          $iterationsNavItemInactive.removeClass('diff-view-old');
-          restoreCodeBlocks();
-        } else {
-          var otherTab = $activeTab.prev('.iterations-nav-item');
-          if (otherTab.length < 1) {
-            otherTab = $activeTab.next('.iterations-nav-item');
-          }
-          otherTab.data('solution').forEach(diffCodeBlock);
-          otherTab.addClass('diff-view-old');
-        }
-        $submission.toggleClass('diff-view');
+        toggleDiffView();
       });
     }
+
+    toggleDiffView = function () {
+      if ($submission.hasClass('diff-view')) {
+        $iterationsNavItemInactive.removeClass('diff-view-old');
+        restoreCodeBlocks();
+      } else {
+        findAdjacentTab()
+        .addClass('diff-view-old')
+        .data('solution').forEach(diffCodeBlock);
+      }
+      $submission.toggleClass('diff-view');
+    };
+
+    // When the diff button is clicked, try to diff the active tab against
+    // the previous iteration. If the active tab is the first iteration, then
+    // diff against the second iteration.
+    findAdjacentTab = function () {
+      var otherTab = $activeTab.prev('.iterations-nav-item');
+      if (otherTab.length < 1) {
+        otherTab = $activeTab.next('.iterations-nav-item');
+      }
+      return otherTab;
+    };
   });
 })();
