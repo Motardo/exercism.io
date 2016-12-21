3945bce5345f47ac76a169a20fa8fd1b8a176581
Nathan Wilson <nswilson@gmail.com>
2016-12-21 15:33:19 -0500

    Add preview of other iterations
    When viewing a submission with multiple iterations, hovering the
    mouse over an inactive iteration's tab will cause that iterations
    code to be displayed in the code view window(s). This is a way to
    quickly see the differences between iterations by hovering back
    and forth between tabs.

---
+++ frontend/app/js/preview.js
@@ -0,0 +1,33 @@
+(function () {
+  "use strict";
+
+  var codeBlockCache,
+      $codeBlocks,
+      $activeTab,
+      $iterationsNavItemInactive;
+
+  $(function() {
+
+    codeBlockCache = [];
+    $codeBlocks = $('.submission-code-body td.code > pre');
+    $activeTab = $('.iterations-nav-item.active');
+    $iterationsNavItemInactive = $('.iterations-nav-item:not(.active)');
+
+    // Copy code blocks to a cache so we can restore syntax highlighting
+    // after previewing another iteration
+    $codeBlocks.each(function () {
+      codeBlockCache.push($(this).html());
+    });
+
+    $iterationsNavItemInactive.hover(function() {
+      $(this).data('solution').forEach(function (file, index) {
+        $codeBlocks.eq(index).text(file[1]);
+      });
+    }, function() {
+      $codeBlocks.each(function (index) {
+        $(this).html(codeBlockCache[index]);
+      });
+    });
+
+  });
+})();
